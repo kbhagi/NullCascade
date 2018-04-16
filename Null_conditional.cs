9ab26ddaef9b464a3a59a84073906e9b9f40109b
bhargavak <bhargavak37@gmail.com>
2018-04-16 20:45:31 +0530

    Update Null_conditional.cs

--- Null_conditional.cs
+++ Null_conditional.cs
@@ -2,5 +2,8 @@ Before
 if(book !=null && book.File != null && book.File.Name != null)
                 return book.File.Name;
 
-After
+After                                                 After After
+if(book?.File?.Name != null)                           return book?.File?.Name ?? string.Empty;
+               return book.File.Name;
+return string.Empty
 
